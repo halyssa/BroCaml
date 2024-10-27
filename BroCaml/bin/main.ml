e42aa4f56a9876b1242ab5b12360adaf79344cb4
Alyssa Hsu <ah2279@cornell.edu>
2024-10-27 18:02:22 -0400

    Implemented driver - user can use contains and search food functions - user can also quit program whenever - rewrote data.ml to access user.ml's eatery

--- BroCaml/bin/main.ml
+++ BroCaml/bin/main.ml
@@ -1,17 +1,62 @@
 open BroCaml.Data
+open BroCaml.User
 open Lwt
 open Cohttp_lwt_unix
 
-let main () =
-  let%lwt eateries = get_data () in
+let run_search_food food eateries =
+  let result = search_food food eateries in
+  if result = [] then
+    Printf.printf "Unfortunately, %s is not served in the eateries today. " food
+  else List.iter (fun p -> Printf.printf "%s\n" p) result
 
-  List.iter
-    (fun eatery ->
-      Printf.printf "Eatery: %s\n" eatery.name;
-      Printf.printf "Menu Items:\n";
-      List.iter (fun item -> Printf.printf "  - %s\n" item) eatery.menu;
-      Printf.printf "\n" (* For better separation between eateries *))
-    eateries;
+let run_contains food eateries =
+  let result = contains food eateries in
+  match result with
+  | true ->
+      Printf.printf
+        "%s is served in the eateries today! Would you like to see where it is \
+         served (y/n)? "
+        food;
+      let response = read_line () in
+      if response = "y" then run_search_food food eateries
+  | false ->
+      Printf.printf "Unfortunately, %s is not served in the eateries today. "
+        food
+
+let quit_program () =
+  print_endline "Thanks for using FindMyFood!";
+  exit 0
+
+let rec prompt_user (eateries : eatery list) =
+  print_endline "Please choose a number that best fits your desired action";
+  print_endline "1. Check if a <food> is served at any of the eateries";
+  (* print_endline "2. Check if a <food> is served at a specific <eatery>"; *)
+  print_endline "2. Search where a <food> is being served";
+  print_endline "3. Quit";
+  let action = read_line () in
+  let parts = String.split_on_char ' ' action in
+  match parts with
+  | [ "1"; food ] ->
+      run_contains food eateries;
+      prompt_user eateries
+  (* | ["2"; food; eatery] -> contains_helper food (create_eatery eatery); *)
+  | [ "2"; food ] ->
+      run_search_food food eateries;
+      prompt_user eateries
+  | [ "3" ] -> quit_program ()
+  | _ ->
+      print_endline
+        "That action does not exist or is incorrectly formatted.";
+      prompt_user eateries
+
+let main () =
+  print_endline "Hey! Welcome to FindMyFood";
+  let%lwt eateries = get_data () in
+  let () = prompt_user eateries in
   Lwt.return_unit
+(* List.iter (fun eatery -> Printf.printf "Eatery: %s\n" eatery.name;
+   Printf.printf "Menu Items:\n"; List.iter (fun item -> Printf.printf " - %s\n"
+   item) eatery.menu; Printf.printf "\n" (* For better separation between
+   eateries *)) eateries; *)
 
 let () = Lwt_main.run (main ())
--- BroCaml/lib/data.ml
+++ BroCaml/lib/data.ml
@@ -1,10 +1,7 @@
 open User
 open Cohttp_lwt_unix
 
-type eatery = {
-  name : string;
-  menu : string list;
-}
+(* type eatery = { name : string; menu : string list; } *)
 (** type [eatery] stores a string [name] and string list [menu]*)
 
 (** [fetch_json] is the json of [url]. Raises:
@@ -19,7 +16,7 @@ let fetch_json url =
     let%lwt response, body = Client.get (Uri.of_string url) in
     let%lwt body_string = Cohttp_lwt.Body.to_string body in
     let json = Yojson.Safe.from_string body_string in
-    print_endline "Successfully fetched and parsed JSON.";
+    (* print_endline "Successfully fetched and parsed JSON."; *)
     Lwt.return json
   with
   | Failure _ -> Lwt.fail_with "HTTP request failed with error"
@@ -30,102 +27,124 @@ let fetch_json url =
 
 (** [parse_eateries] parses the given JSON object [json] and returns an Lwt list
     of eateries. *)
-    let parse_eateries json =
-      let open Yojson.Safe.Util in
-      try
-        match json |> member "data" |> member "eateries" with
-        | `Null -> Lwt.return [] (* Return an empty list if "eateries" is null *)
-        | eateries_json ->
-            let eateries =
-              eateries_json |> to_list
-              |> List.map (fun eatery ->
-                     let name = eatery |> member "name" |> to_string in
-                     let menu_items =
-                       let dining_items =
-                         match eatery |> member "diningItems" with
-                         | `Null -> [] (* Handle case where diningItems is null *)
-                         | items_json -> items_json |> to_list
-                       in
-                       let operating_hours =
-                         match eatery |> member "operatingHours" with
-                         | `Null ->
-                             [] (* Handle case where operatingHours is null *)
-                         | hours_json -> hours_json |> to_list
-                       in
-                       (* Collect all menu items from operating hours and their events *)
-                       let menu_from_events =
-                         operating_hours
-                         |> List.fold_left
-                              (fun acc hour ->
-                                match hour |> member "events" with
-                                | `Null -> acc
-                                | events_json ->
-                                    let events_list = events_json |> to_list in
-                                    (* Extract items from each event's menu *)
-                                    acc
-                                    @ (events_list
-                                      |> List.fold_left
-                                           (fun acc event ->
-                                             match event |> member "menu" with
-                                             | `Null -> acc
-                                             | menu_json ->
-                                                 let menu_list =
-                                                   menu_json |> to_list
-                                                 in
-                                                 (* Extract items from each
-                                                    category's items in the menu *)
-                                                 acc
-                                                 @ (menu_list
-                                                   |> List.fold_left
-                                                        (fun acc category ->
-                                                          match
-                                                            category
-                                                            |> member "items"
-                                                          with
-                                                          | `Null -> acc
-                                                          | items_json ->
-                                                              let items_list =
-                                                                items_json
-                                                                |> to_list
-                                                              in
-                                                              acc
-                                                              @ (items_list
-                                                                |> List.map
-                                                                     (fun item ->
-                                                                       item
-                                                                       |> member
-                                                                            "item"
-                                                                       |> to_string)
-                                                                ))
-                                                        []))
-                                           []))
-                              []
-                       in
-                       if List.length menu_from_events > 0 then menu_from_events
-                       else if List.length dining_items > 0 then
-                         dining_items
+let parse_eateries (json : Yojson.Safe.t) : User.eatery list Lwt.t =
+  try
+    match
+      json
+      |> Yojson.Safe.Util.member "data"
+      |> Yojson.Safe.Util.member "eateries"
+    with
+    | `Null -> Lwt.return [] (* Return an empty list if "eateries" is null *)
+    | eateries_json ->
+        let eateries =
+          eateries_json |> Yojson.Safe.Util.to_list
+          |> List.map (fun eatery ->
+                 let name =
+                   eatery
+                   |> Yojson.Safe.Util.member "name"
+                   |> Yojson.Safe.Util.to_string
+                 in
+                 let menu_items =
+                   let dining_items =
+                     match eatery |> Yojson.Safe.Util.member "diningItems" with
+                     | `Null -> []
+                     | items_json ->
+                         items_json |> Yojson.Safe.Util.to_list
                          |> List.map (fun item ->
-                                item |> member "item" |> to_string)
-                       else
-                         (* If both diningItems and operatingHours/events/menu/items are empty,
-                            look at diningCuisines *)
-                         match eatery |> member "diningCuisines" with
-                         | `Null -> [] (* Handle case where diningCuisines is null *)
-                         | cuisines_json ->
-                             cuisines_json |> to_list
-                             |> List.map (fun cuisine ->
-                                    cuisine |> member "name" |> to_string)
-                     in
-                     { name; menu = menu_items })
-            in
-            Lwt.return eateries
-      with e ->
-        print_endline ("Unexpected error in parse_eateries: " ^ Printexc.to_string e);
-        Lwt.return []
+                                item
+                                |> Yojson.Safe.Util.member "item"
+                                |> Yojson.Safe.Util.to_string)
+                   in
+                   let operating_hours =
+                     match
+                       eatery |> Yojson.Safe.Util.member "operatingHours"
+                     with
+                     | `Null -> []
+                     | hours_json -> hours_json |> Yojson.Safe.Util.to_list
+                   in
+                   let menu_from_events =
+                     operating_hours
+                     |> List.fold_left
+                          (fun acc hour ->
+                            match hour |> Yojson.Safe.Util.member "events" with
+                            | `Null -> acc
+                            | events_json ->
+                                let events_list =
+                                  events_json |> Yojson.Safe.Util.to_list
+                                in
+                                acc
+                                @ (events_list
+                                  |> List.fold_left
+                                       (fun acc event ->
+                                         match
+                                           event
+                                           |> Yojson.Safe.Util.member "menu"
+                                         with
+                                         | `Null -> acc
+                                         | menu_json ->
+                                             let menu_list =
+                                               menu_json
+                                               |> Yojson.Safe.Util.to_list
+                                             in
+                                             acc
+                                             @ (menu_list
+                                               |> List.fold_left
+                                                    (fun acc category ->
+                                                      match
+                                                        category
+                                                        |> Yojson.Safe.Util
+                                                           .member "items"
+                                                      with
+                                                      | `Null -> acc
+                                                      | items_json ->
+                                                          let items_list =
+                                                            items_json
+                                                            |> Yojson.Safe.Util
+                                                               .to_list
+                                                          in
+                                                          acc
+                                                          @ (items_list
+                                                            |> List.map
+                                                                 (fun item ->
+                                                                   item
+                                                                   |> Yojson
+                                                                      .Safe
+                                                                      .Util
+                                                                      .member
+                                                                        "item"
+                                                                   |> Yojson
+                                                                      .Safe
+                                                                      .Util
+                                                                      .to_string)
+                                                            ))
+                                                    []))
+                                       []))
+                          []
+                   in
+                   if List.length menu_from_events > 0 then menu_from_events
+                   else if List.length dining_items > 0 then dining_items
+                   else
+                     match
+                       eatery |> Yojson.Safe.Util.member "diningCuisines"
+                     with
+                     | `Null -> []
+                     | cuisines_json ->
+                         cuisines_json |> Yojson.Safe.Util.to_list
+                         |> List.map (fun cuisine ->
+                                cuisine
+                                |> Yojson.Safe.Util.member "name"
+                                |> Yojson.Safe.Util.to_string)
+                 in
+                 User.create_eatery name menu_items)
+        in
+        Lwt.return eateries
+  with e ->
+    print_endline ("Unexpected error in parse_eateries: " ^ Printexc.to_string e);
+    Lwt.return []
 
 (** [get_data] returns a Lwt list of eateries from
     "https://now.dining.cornell.edu/api/1.0/dining/eateries.json" *)
-let get_data () : eatery list Lwt.t =
+let get_data () : User.eatery list Lwt.t =
   let url = "https://now.dining.cornell.edu/api/1.0/dining/eateries.json" in
   try%lwt
     let%lwt json = fetch_json url in
