5bd75d9ae558e5cfb5d753ee19f0190fe109129b
Jo Raghavan <jbr266@cornell.edu>
2024-12-11 15:57:40 -0500

    created interface file for login compilation unit

---
+++ BroCaml/lib/login.mli
@@ -0,0 +1,29 @@
+exception BindingError of string
+(* [BindingError] is raised when there is an error binding values to SQL
+   statements. *)
+
+val user_exists : Sqlite3.db -> string -> bool
+(** [user_exists db username] checks if a user [username] exists in the database
+    [db]. *)
+
+val validate_user : Sqlite3.db -> string -> string -> bool Lwt.t
+(** [validate_user db username password] validates the user's credentials using
+    [username] and [password] in the database [db] *)
+
+val create_user :
+  finalize:(Sqlite3.stmt -> Sqlite3.db -> unit) ->
+  Sqlite3.db ->
+  string ->
+  string ->
+  unit
+(** [create_user ~finalize db username password] creates a new user with
+    username [username] and password [password] in the database [db]. *)
+
+val fetch_users :
+  finalize:(Sqlite3.stmt -> Sqlite3.db -> Sqlite3.Rc.t) -> Sqlite3.db -> unit
+(** [fetch_users ~finalize db] fetches and prints all users in the database
+    [db]. *)
+
+val connect_db_checked : string -> Sqlite3.db
+(** [connect_db_checked db_file] opens a connection to the database file
+    [db_file] *)
